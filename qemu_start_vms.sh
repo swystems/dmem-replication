478da83d82d63c98c8da7278e38beab592442d1e
daviderovell0 <daviderovelli24@gmail.com>
2025-04-30 15:55:56 +0100

    shared memory QEMU VMs

--- qemu_start_vms.sh
+++ qemu_start_vms.sh
@@ -2,29 +2,32 @@
 # QEMU VM with CXL memory attached as default and only memory.
 # assumes VMs were created on cxlvm{x}_disk.qcow2
 # Assmues CXL memory on the host is on NUMA node 2
+# start in background
+
+truncate -s 16G /dev/shm/ivshmem
+chmod 666 /dev/shm/ivshmem
+
+COMMON_SETTINGS="-machine q35 \
+-machine q35 \
+-cpu host \
+-smp 8 \
+--enable-kvm \
+-object memory-backend-file,size=16G,share=on,mem-path=/dev/shm/ivshmem,host-nodes=2,policy=bind,prealloc=on,id=cxl-mem \
+-device ivshmem-plain,memdev=cxl-mem \
+-m 16G,slots=1,maxmem=32G \
+-daemonize \
+-display none"
 
 # VM1
 sudo qemu-system-x86_64 \
--nographic \
--machine q35 \
--cpu host \
--smp 8 \
---enable-kvm \
--object memory-backend-ram,id=cxl-mem,size=16G,host-nodes=2,policy=bind,prealloc=on \
--m 16G,slots=1,maxmem=32G \
 -drive if=virtio,file=cxlvm1_disk.qcow2,cache=none \
 -net nic,macaddr=52:54:00:12:34:01 \
--net user,hostfwd=tcp::2222-:22 &
+-net user,hostfwd=tcp::2222-:22 \
+${COMMON_SETTINGS} &
 
 # VM2
 sudo qemu-system-x86_64 \
--nographic \
--machine q35 \
--cpu host \
--smp 8 \
---enable-kvm \
--object memory-backend-ram,id=cxl-mem,size=16G,host-nodes=2,policy=bind,prealloc=on \
--m 16G,slots=1,maxmem=32G \
 -drive if=virtio,file=cxlvm2_disk.qcow2,cache=none \
 -net nic,macaddr=52:54:00:12:34:02 \
--net user,hostfwd=tcp::2223-:22 
+-net user,hostfwd=tcp::2223-:22 \
+${COMMON_SETTINGS}
---
+++ shmem_test.c
@@ -0,0 +1,29 @@
+/**
+* @brief Inter-VM communication test using shared memory
+* @note This code is intended to be run in two different VMs (VM1 and VM2)
+*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <string.h>
+
+int main() {
+    // ivshmem host memory device found with lspci. Resource2 = BAR2, the shared 
+    // memory region
+    int fd = open("/sys/bus/pci/devices/0000:00:03.0/resource2", O_RDWR);
+    // mmap to 16 MB
+    char *shmem = mmap(NULL, 16*1024*1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+    // Write data from VM1
+    const char *msg = "Hello from VM1!";
+    memcpy(shmem, msg, strlen(msg) + 1);
+  
+    // Read data in VM2
+    char buffer[32];
+    stncpy(buffer, shmem, sizeof(buffer)); 
+    printf("Received message: %s\n", shmem);
+
+    munmap(shmem, 16*1024*1024);
+    close(fd);
+    return 0;
+}
