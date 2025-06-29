48323b7bd34d2c60a9335d67ce2f1349729cf6ad
daviderovell0 <daviderovelli24@gmail.com>
2025-06-29 18:28:55 +0200

    timer rdtsc test

--- jupyter/round-accuracy/src/bin/timer_test.rs
+++ jupyter/round-accuracy/src/bin/timer_test.rs
@@ -1,7 +1,9 @@
-use round_test::*; 
+use round_test::*;
 
 fn main() {
-     let matches = clap::Command::new("Performance test").version("1.0").about("")
+    let matches = clap::Command::new("Performance test")
+        .version("1.0")
+        .about("")
         .arg(clap::arg!(-s --sleep <SLEEP_TIME> "Time to sleep for in usec.").required(true))
         .arg(clap::arg!(-t --threshold <THRESHOLD> "Threshold").default_value("0"))
         .arg(clap::arg!(-r --ratio <RATIO> "Sleep / busy poll ratio").default_value("1"))
@@ -10,15 +12,21 @@ fn main() {
 
     // Parse the command line arguments
     let sleep_time = matches.get_one::<String>("sleep").unwrap().replace('_', "");
-    let sleep_time =  sleep_time.parse::<u64>().expect("String not parsable");
-    let threshold= matches.get_one::<String>("threshold").unwrap().replace('_', "");
+    let sleep_time = sleep_time.parse::<u64>().expect("String not parsable");
+    let threshold = matches
+        .get_one::<String>("threshold")
+        .unwrap()
+        .replace('_', "");
     let mut threshold = threshold.parse::<u64>().expect("String not parsable");
-    let  ratio = matches.get_one::<String>("ratio").unwrap().replace('_',"");
+    let ratio = matches.get_one::<String>("ratio").unwrap().replace('_', "");
     let ratio = ratio.parse::<f32>().expect("String not parsable");
     let mut durations = Vec::new();
-    let attempts = matches.get_one::<String>("attempts").unwrap().replace('_', "");
+    let attempts = matches
+        .get_one::<String>("attempts")
+        .unwrap()
+        .replace('_', "");
     let attempts = attempts.parse::<usize>().expect("String not parsable");
-    
+
     if sleep_time < threshold {
         panic!("Sleep time must be greater than threshold");
     }
@@ -26,19 +34,20 @@ fn main() {
     if ratio >= 0.0 && ratio < 1.0 {
         // If ratio is set, we use it to calculate the threshold
         // threshold = sleep_time / ratio
-        threshold = (sleep_time as f32 * (1.0-ratio)) as u64;
+        threshold = (sleep_time as f32 * (1.0 - ratio)) as u64;
     }
 
     // println!("ratio: {ratio}, threshold: {threshold}, sleep_time: {sleep_time}");
-    for _ in 0..attempts{
-        let start = std::time::Instant::now();    
-    
-        busy_poll_sleep(sleep_time, threshold);
+    for _ in 0..attempts {
+        let start = std::time::Instant::now();
+
+        // busy_poll_sleep(sleep_time, threshold);
+        let _time = busy_poll_sleep_rdtsc(sleep_time, threshold);
         let end = start.elapsed();
         durations.push(end);
         //println!("Time to sleep {}, Elapsed {:?}", sleep_time, end);
     }
-    
+
     let avg = std::time::Duration::from_secs_f64(mean(&durations));
     let std = std::time::Duration::from_secs_f64(std(&durations));
     let max = durations.iter().max();
@@ -48,5 +57,8 @@ fn main() {
     println!("Std: {:?}", std);
     println!("Max: {:?}", max.unwrap());
     println!("Min: {:?}", min.unwrap());
-    println!("jitter: {:?}us", (max.unwrap().as_nanos() as f32- min.unwrap().as_nanos() as f32)/1000.0);
+    println!(
+        "jitter: {:?}us",
+        (max.unwrap().as_nanos() as f32 - min.unwrap().as_nanos() as f32) / 1000.0
+    );
 }
--- jupyter/round-accuracy/src/lib.rs
+++ jupyter/round-accuracy/src/lib.rs
@@ -1,3 +1,5 @@
+const CPU_FREQ: f64 = 3.6; // 3.6 GHz, adjust as necessary
+
 pub fn get_time_ns() -> u64 {
     let duration = std::time::SystemTime::now()
         .duration_since(std::time::SystemTime::UNIX_EPOCH)
@@ -5,20 +7,23 @@ pub fn get_time_ns() -> u64 {
     duration.as_nanos() as u64
 }
 
-pub fn mean(durations: &[std::time::Duration]) -> f64{
-    let dur = durations.iter().sum::<std::time::Duration>()/ durations.len() as u32;    
+pub fn mean(durations: &[std::time::Duration]) -> f64 {
+    let dur = durations.iter().sum::<std::time::Duration>() / durations.len() as u32;
     dur.as_secs_f64()
 }
 
-pub fn std(durations: &[std::time::Duration]) -> f64{
-    let mean = mean(durations);    
-    let variance = durations.iter().map(|el| {
-            let diff = el.as_secs_f64()- mean;
-            diff*diff
-        }
-     ).sum::<f64>()/durations.len() as f64;
-     variance.sqrt()
-    }
+pub fn std(durations: &[std::time::Duration]) -> f64 {
+    let mean = mean(durations);
+    let variance = durations
+        .iter()
+        .map(|el| {
+            let diff = el.as_secs_f64() - mean;
+            diff * diff
+        })
+        .sum::<f64>()
+        / durations.len() as f64;
+    variance.sqrt()
+}
 
 pub fn percentile(latencies: &Vec<u64>, p: f32) -> u64 {
     if latencies.is_empty() {
@@ -30,17 +35,16 @@ pub fn percentile(latencies: &Vec<u64>, p: f32) -> u64 {
     sorted[index]
 }
 
-
 // busy poll sleep: alternate between sleeping and busy polling
 // sleep for ns - threshold nanoseconds, busy poll for the rest
 #[no_mangle] // display symbol name in perf
-pub fn busy_poll_sleep(mut ns: u64, threshold: u64){
-    if ns==0{ 
+pub fn busy_poll_sleep(mut ns: u64, threshold: u64) {
+    if ns == 0 {
         return;
     }
     let start = std::time::Instant::now();
 
-    if ns > threshold{
+    if ns > threshold {
         let sleep_duration = std::time::Duration::from_nanos(ns - threshold);
         // uses nanosleep() syscall on linux
         std::thread::sleep(sleep_duration);
@@ -48,9 +52,9 @@ pub fn busy_poll_sleep(mut ns: u64, threshold: u64){
         // One might expect the diff to always be 1_000_000 nanoseconds,
         // but given that thread::sleep might sleep for more than the requested time
         // it might not always be the case.
-        let diff =  start.elapsed();
-       // println!("diff {:?}", diff);
-        if diff.as_nanos() as u64 > ns{
+        let diff = start.elapsed();
+        // println!("diff {:?}", diff);
+        if diff.as_nanos() as u64 > ns {
             return;
         }
         ns -= diff.as_nanos() as u64;
@@ -58,9 +62,39 @@ pub fn busy_poll_sleep(mut ns: u64, threshold: u64){
     }
 
     let new_start = get_time_ns();
-    while get_time_ns() - new_start< ns {
+    while get_time_ns() - new_start < ns {
         std::hint::spin_loop();
         //std::thread::yield_now();
     }
-        
+}
+
+#[inline(always)]
+fn rdtsc_ns() -> f64 {
+    unsafe { core::arch::x86_64::_rdtsc() as f64 / CPU_FREQ }
+}
+
+pub fn busy_poll_sleep_rdtsc(mut ns: u64, threshold: u64) -> f64 {
+    if ns == 0 {
+        return 0.0;
+    }
+    let start = rdtsc_ns();
+
+    if ns > threshold {
+        let sleep_duration = std::time::Duration::from_nanos(ns - threshold);
+        std::thread::sleep(sleep_duration);
+        let diff = rdtsc_ns() - start;
+        if diff > ns as f64 {
+            return diff;
+        }
+        ns -= diff as u64;
+    }
+
+    let mut diff = 0.0;
+    let new_start = rdtsc_ns();
+    while diff < ns as f64 {
+        std::hint::spin_loop();
+        diff -= rdtsc_ns() - new_start;
+    }
+
+    diff
 }
